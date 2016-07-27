d98828726d0af4c3c5091a20903669d203b7f369
Local User <LocalUser@Generic-IDevice.local>
2016-07-26 20:58:15 -0500

    added intelij keybindings

--- .hyperterm.js
+++ .hyperterm.js
@@ -74,7 +74,7 @@ module.exports = {
     'hyperterm-transparent-bg',
     'hyperterm-visor',
     'hyperterm-open-devtools',
-    'hyperterm-close-on-left', 'drip' ],
+    'hyperterm-close-on-left', 'drip', 'hyperpower' ],
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
